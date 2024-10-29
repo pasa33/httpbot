a020fdb499903728c633f7899257b12e84736341
Pasa33 <andrea.pasinelli33@gmail.com>
2024-10-29 09:44:32 +0100

    MakeReturnRequest

--- request.go
+++ request.go
@@ -24,7 +24,10 @@ func (bot *HttpBot) MakeRequest(method, url string, headers []Header, payload ..
 	req.Header = bot.generateHeaders(headers)
 	res, err := bot.client.Do(req)
 	if err != nil {
-		log.Printf("%+v", err)
+		err, parsed := parseRequestError(err)
+		if !parsed {
+			log.Printf("%+v", err)
+		}
 	}
 
 	return res, err
@@ -54,6 +57,29 @@ func (bot *HttpBot) MakeRequestCustomOrder(method, url string, headers []Header,
 	return bot.client.Do(req)
 }
 
+func (bot *HttpBot) MakeReturnRequest(method, url string, headers []Header, payload ...[]byte) (reqq *http.Request, ress *http.Response, err error) {
+
+	var body io.Reader = nil
+	if len(payload) > 0 && len(payload[0]) > 0 {
+		body = bytes.NewBuffer(payload[0])
+	}
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return req, nil, err
+	}
+
+	req.Header = bot.generateHeaders(headers)
+	res, err := bot.client.Do(req)
+	if err != nil {
+		err, parsed := parseRequestError(err)
+		if !parsed {
+			log.Printf("%+v", err)
+		}
+	}
+
+	return req, res, err
+}
+
 func EncodeJSON(j map[string]interface{}) []byte {
 	jsonData, _ := json.Marshal(j)
 	return jsonData
@@ -75,4 +101,18 @@ func EncodeURLForm(j map[string]interface{}) []byte {
 
 func DecompressBody(res *http.Response) io.ReadCloser {
 	return http.DecompressBody(res)
+}
+
+func parseRequestError(err error) (error, bool) {
+	str := strings.ToLower(err.Error())
+	if strings.Contains(str, `client.timeout exceeded while awaiting headers`) {
+		return fmt.Errorf("timeout exceeded"), true
+	}
+
+	if strings.Contains(str, `proxy responded with non 200 code:`) {
+		code := strings.Split(str, "proxy responded with non 200 code:")[1]
+		return fmt.Errorf("proxy error: %s", code), true
+	}
+
+	return err, false
 }
