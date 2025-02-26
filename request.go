e62c54d3e032079fb690a4784012bbbef958cb56
Pasa33 <andrea.pasinelli33@gmail.com>
2025-02-26 14:40:55 +0100

    add PrepareRequest

--- request.go
+++ request.go
@@ -10,6 +10,22 @@ import (
 	http "github.com/bogdanfinn/fhttp"
 )
 
+func (bot *HttpBot) PrepareRequest(method, url string, headers []Header, payload ...[]byte) (reqq *http.Request, err error) {
+
+	var body io.Reader = nil
+	if len(payload) > 0 && len(payload[0]) > 0 {
+		body = bytes.NewBuffer(payload[0])
+	}
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header = bot.generateHeaders(headers)
+
+	return req, err
+}
+
 func (bot *HttpBot) MakeRequest(method, url string, headers []Header, payload ...[]byte) (red *http.Response, err error) {
 
 	var body io.Reader = nil
